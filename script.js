476a6d2d48be805ac71b06fe4ec024b3486d9bb4
MikhailVolvach <mihailvolvach@gmail.com>
2023-02-09 09:36:04 +0300

    Done

---
+++ script.js
@@ -0,0 +1,14 @@
+// const calculatorButtons = document.querySelector(".calculator__buttons");
+//
+// let result = 0;
+// calculatorButtons.addEventListener("click", (event) => {
+//
+// });
+//
+// // console.log(calculatorButtons);
+
+const backspace = document.querySelector(".calculator__button--backspace");
+
+backspace.addEventListener('click', (event) => {
+
+});
