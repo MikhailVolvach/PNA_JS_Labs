8c2c2fa0ca97543d1c77d1eec1ca8381d6d2cab6
MikhailVolvach <mihailvolvach@gmail.com>
2023-02-09 11:29:16 +0300

    Первое задание

--- task1.ts
+++ task1.ts
@@ -3,11 +3,21 @@
 // необходимо оставить значение, которое встретилось раньше.
 
 const merge = (...args: object[]): object => {
-    let result: object = {};
+    let result = new Map();
     let arg: object = {};
+
     for (arg of args) {
-        // Object.entries(arg)
+        Object.entries(arg).forEach(([argKey, argValue]) => {
+            if (!result.has(argKey)) {
+                result.set(argKey, argValue);
+            }
+        });
     }
 
     return result;
 }
+
+const obj1 = {1: "F", 2: "A"}
+const obj2 = {1: "ADA", 3: "adfadfa"}
+const obj3 = {23: "afaf"};
+console.log(merge(obj1, obj2, obj3));
