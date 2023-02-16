9e2c79123e9a953c550f3045543bf2048d06b77f
MikhailVolvach <mihailvolvach@gmail.com>
2023-02-16 11:42:57 +0300

    Начал делать 11

---
+++ task11.ts
@@ -0,0 +1,10 @@
+const maxLengthOfOne = (str: string) => {
+
+    for (let i = 0; i < ; i++) {
+        
+    }
+    
+    return 7;
+}
+
+console.log(maxLengthOfOne('1000000111100011111010111101111111'));
