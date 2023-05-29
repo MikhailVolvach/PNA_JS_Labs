41152484ff2539932899d2e0e04f0763ffe70074
MikhailVolvach <80108721+MikhailVolvach@users.noreply.github.com>
2023-05-29 10:48:55 +0300

    Delete constants.js

--- frontend/src/modules/constants.js
+++
@@ -1,6 +0,0 @@
-export const Constants = {
-    GROUP_ID: "club219649240",
-    ACCESS_TOKEN: "vk1.a.gPcyBTBZBIZLc7SjMaCkxEe7x1KvuvcgpaAl6KgKcj31hmQPn1WiRvsMqpZUw0GMoFyDN93mAtgxrRzjEHXr2UapMBk9W4kNDxvUdGeFjEj6kl_ksj0wuMJruNtJ8OSknebpQRYLhav-31oSKaK-g2X-ML5ITylIaFLhWrBLeXl_bqWjVw91SbTU2GPjTilnDRQIFvwQuOUerOZV4RMedQ",
-    ACCESS_TOKEN_USER: "vk1.a.pD7SGxTDQcfDW6ZsJZmkoogJ48nPPsNLlb-1-_MB1-XAqjf3G_gvBdIXAlxrMzuWu8mc03VCtV27SKAOesm97r19tkDx_G4-2tsIOLppiPcBXMDIxezlLmeF2tM9Kx2cWJ3cxhZRWtuvLqSFlbOES-utq5FujurvPwR9fBZoKZocU_PnAuwiXfphI7bbirMO_eDmOV2h1CwfPo9m0LIsJg",
-    VERSION: "5.131"
-}
